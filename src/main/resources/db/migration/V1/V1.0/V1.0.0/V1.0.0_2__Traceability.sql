81665944ff9ad708aa7d679031812a526e5c0e17
themarioga <marioga2512@gmail.com>
2026-08-28 19:06:58 +0200

    Eliminadas migraciones antiguas

--- src/main/resources/db/migration/V1/V1.0/V1.0.0/V1.0.0_2__Traceability.sql
+++
@@ -1,9 +0,0 @@
--- v1.0.0_2 - Traceability
-
--- Basic tables
-ALTER TABLE t_user ADD creation_date TEXT;
-ALTER TABLE t_room ADD creation_date TEXT;
-
--- Advanced tables
-ALTER TABLE t_game ADD creation_date TEXT;
-ALTER TABLE t_player ADD creation_date TEXT;
